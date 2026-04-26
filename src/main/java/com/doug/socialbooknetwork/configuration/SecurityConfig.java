7f4ace2fa482f8535690d32b9c19f87d8bde4340
douglanyabasa <douglasnyabasa400@gmail.com>
2026-04-26 09:04:42 +0200

    added the jwt secuity config updated

--- src/main/java/com/doug/socialbooknetwork/configuration/SecurityConfig.java
+++ src/main/java/com/doug/socialbooknetwork/configuration/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 .sessionManagement(session-> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 
 
